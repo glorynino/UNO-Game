9fb5c75d154dd51a8e04b389261a605e434b40dc
its.Xero <152559421+its-Xero@users.noreply.github.com>
2024-12-22 00:34:09 +0100

    Add files via upload

---
+++ src/projectpoo/Skip.java
@@ -0,0 +1,13 @@
+package projectpoo;
+public class Skip extends Card{
+    public Skip(String couleur){
+        super(-1,couleur,"Skip");
+    }
+
+    @Override
+    public String toString() {
+        return "type : Skip" +
+                "couleur"+getCouleur();
+    }
+
+}
